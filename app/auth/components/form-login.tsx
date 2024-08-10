66e3d8dd36f8a695b05e8e2c72e19b2e99f5df69
Djonny Nogueira <djonnynogueira@gmail.com>
2024-08-10 14:09:51 -0300

    Estilização formulário.

--- app/auth/components/form-login.tsx
+++ app/auth/components/form-login.tsx
@@ -6,22 +6,22 @@ const LoginForm = () => {
     const [error, setError] = useState()
 
     return (
-        <div>
-            <form>
-                <h1>Login</h1>
-                <p>Faça login para continuar.</p>
+        <div className="w-full h-screen flex items-center justify-center">
+            <form className="p-10 border rounded-lg w-96">
+                <h1 className="text-3x1 font-bold mb-4">Login</h1>
+                <p className="text-sm text-slate-700 mb-10">Faça login para continuar.</p>
 
-                <div>
-                    <div>
+                <div className="flex flex-col">
+                    <div className="flex flex-col gap-1 mb-6 w-full">
                         <label htmlFor="email">E-mail</label>
                         <input type="text" name="email" onChange={() => { }} className="border rounded w-fit p-3" />
                     </div>
-                    <div>
+                    <div className="flex flex-col gap-1 mb-6 w-full">
                         <label htmlFor="password">Senha</label>
                         <input type="text" name="password" onChange={() => { }} className="border rounded w-fit p-3" />
                     </div>
                     {error && <span className="text-rede-400 text-sm block mt-2 ">{error}</span>}
-                    <button type="submit" className="mt-10 br-rose-950 text-slate-50 p-3 rounded">Entrar</button>
+                    <button type="submit" className="mt-10 bg-rose-950 text-slate-50 p-3 rounded">Entrar</button>
                 </div>
             </form>
         </div>
--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,13 +1,10 @@
 import { LoginForm } from "../components/form-login";
 
 
-export default function LoginPage () {
+export default function LoginPage() {
     return (
-        <div className="h-screen flex items-center justify-center">
-            <LoginForm>
-
-            </LoginForm>
-
+        <div>
+            <LoginForm />
         </div>
     )
 }
